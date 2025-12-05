cf62e6c347758bc04a1d56f93f8d2b7ce7014ced
Fady <fadybsuony@gmail.com>
2025-12-05 21:52:57 +0200

    supabase pipeline

---
+++ new_grad/lib/pages/welcome_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/welcome.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(color: Colors.white.withOpacity(0.5)),
+
+          const Positioned(
+            left: 40,
+            top: 80,
+            child: Text(
+              "Hello,",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ),
+
+          const Positioned(
+            left: 40,
+            top: 200,
+            child: Text(
+              "Our legacy, \nyour journey.",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ),
+
+          const Positioned(
+            left: 120,
+            top: 700,
+            child: Text(
+              "Ready to start?",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ),
+
+          Positioned(
+            left: 120,
+            top: 740,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+              ),
+              child: const Text(
+                "Let's Go",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
